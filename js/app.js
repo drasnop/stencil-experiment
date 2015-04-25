2e564d1896ca2877419f345097d16385bdfd0d14
drasnop <ponsard.antoine@gmail.com>
2015-04-25 12:10:38 -0700

    Added basic page "Create wunderlist account"

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
 var state = {
-   "page": 3
+   "page": 1,
+   "email": ""
 }
 
-
 var app = angular.module('stencil-experiment', []);
+
+app.run(function($window){
+   $window.state.email=generateRandomString(8) + "@gmail.com";
+})
+
+generateRandomString = function(length) {
+   var string = "";
+   var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+   for(var i = 0; i < length; i++)
+      string += possible.charAt(Math.floor(Math.random() * possible.length));
+
+   console.log(string)
+   return string;
+}
 
 app.controller('MainCtrl', ['$scope', '$window', function($scope, $window) {
    $scope.state = $window.state;
