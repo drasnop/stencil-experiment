99aa4837f30f407fbbbd06c8bbb444a9c38abe45
drasnop <ponsard.antoine@gmail.com>
2015-05-20 11:58:51 -0700

    Create new participant in Firebase only when consent form clicked

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,50 @@
-app.controller('MainCtrl', ['$scope', '$window', function($scope, $window) {
+app.controller('MainCtrl', ['$scope', '$window', '$http', function($scope, $window, $http) {
    $scope.state = $window.state;
-
    $scope.sslSuccess = false;
+
+   /* when users click "accepct consent form", initialize everything */
+
+   $scope.initializeParticipant = function() {
+
+      // generate a unique ID for each participant
+      if ($window.state.email === "")
+         $window.state.email = generateRandomString(8);
+      console.log("participant email:", $window.state.email)
+
+      // randomly assign participant to one interface and with opposite defaults or not
+      state.condition = {
+         "interface": 3,
+         //"interface": Math.floor(Math.random() * 4),
+         "oppositeDefaults": (Math.random() < 0.5 ? false : true)
+      }
+
+      // store the ID + the condition in firebase (will be checked from my software on wunderlist.com)
+      state.firebase = new Firebase("https://incandescent-torch-4042.firebaseio.com/stencil-experiment/mturk/" + state.email);
+      state.firebase.child("/condition").set(state.condition)
+
+      // retrieve bookmarklet code
+      $http.get("bookmarklets/bookmarklet-setup.js").success(function(data) {
+         console.log("bookmarklet-setup retrieved successfully");
+         if (state.serverURL === "localhost:8888")
+            state.bookmarkletCode = data.replace("tequila.cs.ubc.ca/stencil", "localhost:8888");
+         else
+            state.bookmarkletCode = data;
+      })
+
+      // helper
+      function generateRandomString(length) {
+         var string = "";
+         var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+         for (var i = 0; i < length; i++)
+            string += possible.charAt(Math.floor(Math.random() * possible.length));
+
+         return string;
+      }
+   }
+
+
+   /* basic utilities function to manage progress */
 
    $scope.printConsentForm = function() {
       $window.print();
