6fcf909077cb7297243a4a15b7381820ff683b78
drasnop <ponsard.antoine@gmail.com>
2015-08-05 21:26:50 -0700

    minor: empty page when non authorized browser

--- js/app.js
+++ js/app.js
@@ -11,7 +11,8 @@ app.run(['$window', '$http', '$q', function($window, $http, $q) {
 
    // abort if browser is not firefox or chrome
    if (Browser.name != "Chrome" && Browser.name != "Firefox") {
-      alert("Fatal error! This experiment only works in Google Chrome or Mozilla Firefox. You won't be able to collect your reward. Please use on of these web browsers, or return this HIT.")
+      $("body").empty();
+      alert("Fatal error! This experiment only works in Google Chrome or Mozilla Firefox. You won't be able to collect your reward. Please use one of these web browsers, or return this HIT.")
       return;
    }
 
